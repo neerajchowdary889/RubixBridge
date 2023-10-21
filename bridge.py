d85ac39e9f7c5eed6da98d2c5c8a3268a54558f0
saishibunb <saishibu@jupitermeta.io>
2023-10-21 10:33:59 +0530

    working

--- bridge.py
+++ bridge.py
@@ -1,16 +1,22 @@
 import requests
 
 # Define the API endpoint URL
-api_url = 'http://localhost:20000/api/createdid'  # Replace with the actual API URL
+url = 'http://localhost:20000/api/createdid'
 
-# Define the form data as a dictionary
-form_data = {'did_config': {'type':0, 'dir':'', 'config':'', 'master_did':'', 'secret':'My DID Secret', 'priv_pwd':'mypassword', 'quorum_pwd':'mypassword','img_file':r"image.png", 'priv_img_file':'',  'pub_key_file':'', 'priv_key_file':'', 'quorum_pub_key_file':'', 'quorum_priv_key_file':''}}
+# Create a dictionary for form data
+form_data = {
+    'did_config': (None, '{"type":0,"dir":"","config":"","master_did":"","secret":"My DID Secret","priv_pwd":"mypassword","quorum_pwd":"mypassword"}'),
+}
 
-# Send a POST request with the form data
+# Specify the file to upload
+files = {
+    'img_file': ('image.png', open('image.png', 'rb'), 'image/png')
+}
+
+# Send a POST request with multipart/form-data
 try:
-    response = requests.post(api_url, data=form_data)
-    # response = requests.post(api_url)
-
+    response = requests.post(url, data=form_data, files=files)
+    
     # Check the response status code
     if response.status_code == 200:
         # Request was successful
