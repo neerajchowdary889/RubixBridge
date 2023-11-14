be51bc7ad1b1952cc615b525074495684ed9220e
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-11-14 17:10:26 +0530

    Update startupnode.sh

--- startupnode.sh
+++ startupnode.sh
@@ -2,5 +2,5 @@
 logfile="/home/ubuntu/cronlog/rubixstartup.log"
 #screen -dmS node2 -L -Logfile $log_file $runfile
 echo "Starting session" >> $logfile
-screen -dmS node1 /home/ubuntu/rubix/rubixgoplatform/linux/rubixgop>
+screen -dmS node1 /home/ubuntu/rubix/rubixgoplatform/linux/rubixgoplatform run -p node1 -n 0 -s -port 20000 -testNet -grpcPort 10500
 echo "Started session" >> $logfile
