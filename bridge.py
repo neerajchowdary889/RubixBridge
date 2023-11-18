840162803c1953ffdefe8d5caff090338cdf16d8
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-11-18 15:18:46 +0530

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -27,89 +27,80 @@ def security(fname):
 #CreateDID Parent API
 @app.route('/api/createparentdid', methods=['GET'])
 def createParentDID():
-    security(str(sys._getframe().f_code.co_name))
-    print("createDID API")
-    mongo_client = MongoClient(MONGO_HOST, MONGO_PORT)
+	security(str(sys._getframe().f_code.co_name))
+	print("createDID API")
+	mongo_client = MongoClient(MONGO_HOST, MONGO_PORT)
 	db = mongo_client[MONGO_DB]
 	MONGO_COLLECTION = "parentdid"
 	collection = db[MONGO_COLLECTION]
 	collection.insert_one(mqttData)
 	mongo_client.close()
-    start_time = time.time()
+	start_time = time.time()
 
     # Get the user input for the field (e.g., "AM" or "ISK")
-    user_input = request.args.get('app', '')
+	user_input = request.args.get('app', '')
 
     # Define a dictionary to map field values to ports
-    field_to_port = {
-        'AM': 20000,
-        'ISK': 20001,
-        'V1': 20002,
-        'V2': 20003,
-        'V3': 20004,
-        'V4': 20005,
-        'V5': 20006,
+	field_to_port = {
+		'AM': 20000,
+		'ISK': 20001,
+		'V1': 20002,
+		'V2': 20003,
+		'V3': 20004,
+		'V4': 20005,
+		'V5': 20006,
+	}
         # Add more field-to-port mappings as needed
-    }
+		
 
     # Default port (if the user input is not recognized)
-    default_port = 2
+	default_port = 2
 
     # Get the port based on user input; use the default if not found in the dictionary
-    port = field_to_port.get(user_input, default_port)
+	port = field_to_port.get(user_input, default_port)
     # Check if the user input is not recognized
-    if port == default_port:
-        error_message = f"Invalid Application: {user_input}."
-        return jsonify({'error': error_message}), 400  # Return a JSON error response with a 400 status code
+	if port == default_port:
+		error_message = f"Invalid Application: {user_input}."
+		return jsonify({'error': error_message}), 400  # Return a JSON error response with a 400 status code
     
     # Define the API endpoint URL
-    url = f'http://localhost:{port}/api/createdid'
+	url = f'http://localhost:{port}/api/createdid'
     
     # Create a dictionary for form data
-    form_data = {'did_config': (None, '{"type":0,"dir":"","config":"","master_did":"","secret":"My DID Secret","priv_pwd":"mypassword","quorum_pwd":"mypassword"}'),}
+	form_data = {'did_config': (None, '{"type":0,"dir":"","config":"","master_did":"","secret":"My DID Secret","priv_pwd":"mypassword","quorum_pwd":"mypassword"}'),}
 
     # Specify the file to upload
-    files = {'img_file': ('image.png', open(r'image.png', 'rb'), 'image/png')}
+	files = {'img_file': ('image.png', open(r'image.png', 'rb'), 'image/png')}
 
 # Send a POST request with multipart/form-data
-    try:
-        response = requests.post(url, data=form_data, files=files)
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print(elapsed_time)
+	try:
+		response = requests.post(url, data=form_data, files=files)
+		end_time = time.time()
+		elapsed_time = end_time - start_time
+		print(elapsed_time)
     # Check the response status code
-        if response.status_code == 200:
+		if response.status_code == 200:
         # Request was successful
-            message = json.loads(response.text)
-            if message['status'] == True:
-                print(message['result']['did'])
-                print(message['result']['peer_id'])
-                didpeerid={'status':True,
-                           'createTime':end_time,
-                           'port':port,
-                           'did':message['result']['did'],
-                           'peerid':message['result']['peer_id'],
-                           'timeTaken':elapsed_time,
-                           'creatorIP':request.environ['REMOTE_ADDR'],
-                           'creatorLocation':geocoder.ip(str(request.environ['REMOTE_ADDR'])).city
-                          }
-                collection.insert_one(didpeerid)
-                mongo_client.close()
-                
-                return didpeerid
-            else:
-                print(message)
-                return message
-        else:
-            print(f"POST request failed with status code {response.status_code}")
-            print("Response content:", response.text)
-            return response.text
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        return (str(e))
+			message = json.loads(response.text)
+			if message['status'] == True:
+				print(message['result']['did'])
+				print(message['result']['peer_id'])
+				didpeerid={'status':True,'createTime':end_time,'port':port,'did':message['result']['did'],'peerid':message['result']['peer_id'],'timeTaken':elapsed_time,'creatorIP':request.environ['REMOTE_ADDR'],'creatorLocation':geocoder.ip(str(request.environ['REMOTE_ADDR'])).city}
+				collection.insert_one(didpeerid)
+				mongo_client.close()
+				return didpeerid
+			else:
+				print(message)
+				return message
+		else:
+			print(f"POST request failed with status code {response.status_code}")
+			print("Response content:", response.text)
+			return response.text
+	except requests.exceptions.RequestException as e:
+		print(f"An error occurred: {e}")
+		end_time = time.time()
+		elapsed_time = end_time - start_time
+		return (str(e))
     
 #CreateDID Child API
 @app.route('/api/createchilddid', methods=['GET'])
