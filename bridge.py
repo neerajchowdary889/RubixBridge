82b5040666e356b5cd11565e686a123675970bab
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-11-18 17:11:04 +0530

    Update bridge.py

--- bridge.py
+++ bridge.py
@@ -84,7 +84,14 @@ def createParentDID():
 			if message['status'] == True:
 				print(message['result']['did'])
 				print(message['result']['peer_id'])
-				didpeerid={'status':True,'createTime':end_time,'port':port,'did':str(message['result']['did']),'peerid':str(message['result']['peer_id']),'timeTaken':elapsed_time,'creatorIP':request.environ['REMOTE_ADDR']}
+				didpeerid={'status':True,
+					   'createTime':end_time,
+					   'port':port,
+					   'did':str(message['result']['did']),
+					   'peerid':str(message['result']['peer_id']),
+					   'timeTaken':elapsed_time,
+					   'creatorIP':str(request.environ['REMOTE_ADDR'])
+					  }
 				print(didpeerid)
 				collection.insert_one(didpeerid)
 				mongo_client.close()
