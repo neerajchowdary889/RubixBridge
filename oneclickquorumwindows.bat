9577440c782fac968529ef9217ee7a72fcd817c2
saishibunb <144234378+saishibunb@users.noreply.github.com>
2023-11-20 11:26:23 +0530

    Update oneclickquorumwindows.bat

--- oneclickquorumwindows.bat
+++ oneclickquorumwindows.bat
@@ -1,6 +1,6 @@
 @echo off
 setlocal enabledelayedexpansion
-set "logfile=rubixstartup.log"
+set "logfile=C:\Users\ubuntu\cronlog\rubixstartup.log"
 
 rem Determine the location of the batch script
 for %%F in ("%~dp0") do set "script_dir=%%~dpF"
@@ -19,8 +19,8 @@ cd /d "!rubix_dir!" || exit /b 1
 for /l %%i in (1,1,7) do (
   set /a "port=base_port + %%i - 1"
   set /a "grpc_port=base_grpc_port + %%i - 1"
-  start /b screen -dmS "node%%i" .\rubixgoplatform.exe run -p "node%%i" -n %%i -s -port !port! -testNet -grpcPort !grpc_port!
-  echo Started session for node%%i, port: !port!, grpcPort: !grpc_port! >> !logfile!
+  start "Node%%i" cmd /c ".\rubixgoplatform.exe run -p Node%%i -n %%i -s -port !port! -testNet -grpcPort !grpc_port!"
+  echo Started session for Node%%i, port: !port!, grpcPort: !grpc_port! >> !logfile!
 )
 
 echo All sessions started >> !logfile!
