3fe411b624d532e383ec6f32ab4052a702d19571
Sai Shibu N B <saishibu@jupitermeta.io>
2023-11-20 15:44:11 +0530

    windows code fix

--- oneclickquorumwindows.bat
+++ oneclickquorumwindows.bat
@@ -1,12 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
-set "logfile=C:\Users\ubuntu\cronlog\rubixstartup.log"
+set "logfile=rubixstartup.log"
 
 rem Determine the location of the batch script
 for %%F in ("%~dp0") do set "script_dir=%%~dpF"
 
 rem Navigate to the rubixgoplatform\linux directory
-set "rubix_dir=!script_dir!\..\rubix\rubixgoplatform\linux"
+set "rubix_dir=C:\Users\saishibu\Documents\rubixgoplatform-SC\rubixgoplatform-develop\windows"
 
 echo Starting sessions >> !logfile!
 
@@ -16,10 +16,11 @@ set "base_grpc_port=10500"
 rem Change directory to rubixgoplatform directory
 cd /d "!rubix_dir!" || exit /b 1
 
-for /l %%i in (1,1,7) do (
-  set /a "port=base_port + %%i - 1"
-  set /a "grpc_port=base_grpc_port + %%i - 1"
+for /l %%i in (0,1,6) do (
+  set /a "port=base_port + %%i"
+  set /a "grpc_port=base_grpc_port + %%i"
   start "Node%%i" cmd /c ".\rubixgoplatform.exe run -p Node%%i -n %%i -s -port !port! -testNet -grpcPort !grpc_port!"
+  timeout /t 10 
   echo Started session for Node%%i, port: !port!, grpcPort: !grpc_port! >> !logfile!
 )
 
